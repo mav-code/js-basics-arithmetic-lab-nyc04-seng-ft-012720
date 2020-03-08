52da781d96245b0c725941ee659ceb3eccd9e956
mav-code <mavisansmembres@gmail.com>
2020-03-08 19:50:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+let var1 = 31
+
+let var2 = 2
+
+let multiply = var1 * var2
+
+let random = Math.random * 10000000000000000
+
+let mod = 10 % 6
+
+let max = Math.max(1, 20)
